c08a28cdb79a48b04b7cf2d2dc6024605b0b5a13
Estevao <estevao.vasques@inovan.do>
2021-06-26 15:03:06 -0300

    acessando a sala e fazendo perguntas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,15 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth()
+  const [roomCode, setRoomCode] = useState('')
 
   async function handleCreateRoom() {
     if (!user) {
@@ -22,6 +25,24 @@ export function Home() {
     }
 
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    console.log({ roomCode })
+
+    if (roomCode.trim() === '') return
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists')
+      return
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
 
   return (
@@ -46,10 +67,12 @@ export function Home() {
             ou entre em uma sala
           </div>
 
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
-              placeholder="Digite o código da sala" />
+              placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode} />
 
             <Button type="submit"> Entrar na sala</Button>
           </form>
